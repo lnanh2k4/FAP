ea6ad0deeff02a95378f74513d1635615a170559
HoangTyy <mrmastervp123@gmail.com>
2024-07-03 09:35:56 +0700

    Update Week, Add SQL

--- FAP/src/main/java/utils/SQL.java
+++ FAP/src/main/java/utils/SQL.java
@@ -3,11 +3,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package utils;
-
+import java.sql.*;
 /**
  *
- * @author CE181767 - Le Nhut Anh
+ * @author CE180191 - Huynh Hoang Ty
  */
 public class SQL {
+    private static final String JDBC_URL = "jdbc:sqlserver://localhost;databaseName=FAPDB";
+    private static final String USERNAME = "sa";
+    private static final String PASSWORD =  "sa";
     
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+    }
+    
+    public static void closeConnection(Connection conn){
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void closeStatement(Statement stmt){
+        if (stmt != null){
+            try {
+                stmt.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    
+    public static void setParameters(PreparedStatement ps, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++){
+            ps.setObject(i + 1, params[i]);
+        }
+    }
+    
+    public static ResultSet executeQuery(String query, Object... params) throws SQLException{
+        Connection conn = getConnection();
+        PreparedStatement ps = conn.prepareStatement(query);
+        setParameters(ps, params);
+        return ps.executeQuery();
+    }
 }
