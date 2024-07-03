f623b98a2157fd910abb82df9e4605d1874a5a0c
lnanh2k4 <lnanh2k4@gmail.com>
2024-07-03 15:13:58 +0700

    modified some files

--- FAP/src/main/java/daos/GroupSubjectDAO.java
+++ FAP/src/main/java/daos/GroupSubjectDAO.java
@@ -4,7 +4,6 @@
  */
 package daos;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,12 +20,17 @@ import utils.SQL;
  * @author Nguyen Le Khac Vu - CE180175
  */
 public class GroupSubjectDAO {
-
-    GroupSubject gs;
+    
     int groupSubjectID;
-    Subject subjectID;
-    Group groupID;
-
+    String subjectID;
+    String subjectName;
+    int subjectNoCredit;
+    String subjectPrerequisite;
+    String subjectDescription;
+    int groupID;
+    String groupName;
+    String semesterID;
+    
     public List<GroupSubject> getAllList() {
         ResultSet rs = null;
         List<GroupSubject> list = new ArrayList<>();
@@ -34,28 +38,32 @@ public class GroupSubjectDAO {
         try {
             rs = SQL.executeQuery(query);
             while (rs.next()) {
-                gs = (GroupSubject) rs.getObject("gs");
                 groupSubjectID = rs.getInt("groupSubjectID");
-                subjectID = (Subject) rs.getObject("subjectID");
-                groupID = (Group) rs.getObject("groupID");
-                list.add(new GroupSubject(groupSubjectID, subjectID, groupID));
+                subjectID = rs.getString("subjectID");
+                subjectName = rs.getString("subjectName");
+                subjectNoCredit = rs.getInt("subjectNoCredit");
+                subjectPrerequisite = rs.getString("subjectPrerequisite");
+                subjectDescription = rs.getString("subjectDescription");
+                groupID = rs.getInt("groupID");
+                groupName = rs.getString("groupName");
+                semesterID = rs.getString("semesterID");
+                list.add(new GroupSubject(groupSubjectID, new Subject(subjectID, subjectName, subjectNoCredit, subjectPrerequisite, subjectDescription), new Group(groupID, groupName, semesterID)));
             }
         } catch (SQLException ex) {
             Logger.getLogger(GroupSubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(GroupSubjectDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+        
         return list;
     }
     
-    public int addGroupSubject(String subjectID, int groupID){
-        int rs=-1;
-
-            String query="";
-            
-
+    public int addGroupSubject(String subjectID, int groupID) {
+        int rs = -1;
+        
+        String query = "";
+        
         return rs;
     }
-
+    
 }
--- FAP/src/main/java/daos/GroupSubjectStudentDAO.java
+++ FAP/src/main/java/daos/GroupSubjectStudentDAO.java
@@ -9,5 +9,23 @@ package daos;
  * @author Nguyen Le Khac Vu - CE180175
  */
 public class GroupSubjectStudentDAO {
+
+    int gssID;
+    String userID;
+    String firstName;
+    String lastName;
+    int sex;
+    String email;
+    String phone;
+    int semester;
+    int role;
+    String password;
+    String curriculumID;
+    String curriculumName;
+    String specializationID;
+    String specializationName;
+    String majorID;
+    String majorName;
+    
     
 }
